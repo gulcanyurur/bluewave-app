4b84ebad5ed39aad9db51aaf9ae5d4b34262c23f
Gülcan yürür <gulcanyurur92@gmail.com>
2025-07-13 09:39:15 +0300

    Dinamik slug yapısı eklendi

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 
-const BlogSlugPage = () => {
+import React from 'react';
+
+const BlogSlugPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const slug = (await params).slug;
+
   return (
-    <div>
-      BlogSlugPage
-    </div>
-  )
-}
+    <div>{slug}</div>
+  );
+};
 
-export default BlogSlugPage
+export default BlogSlugPage;
